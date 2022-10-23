c250ed9a3e7ebee60d1171a678abbc4dddd749fa
MichalZelazko <242149@edu.p.lodz.pl>
2022-10-24 00:29:43 +0200

    final version

--- Operator Overload Stack/Stack.cpp
+++ Operator Overload Stack/Stack.cpp
@@ -1,99 +1,3 @@
-/*#include "Stack.h"
-#include <iostream>
-#include <cstring>
-
-using namespace std;
-
-Stack::Stack()
-{
-    this->top = 0;
-    this->size = 0;
-    this->items = 0;
-}
-
-Stack::~Stack()
-{
-    free(this->items);
-}
-
-Stack::Stack(const Stack& s)
-{
-    this->top = s.top;
-    cout << "Copied top: " << this->top << endl;
-    this->size = s.size;
-    cout << "Copied size: " << this->size << endl;
-    this->items = (int*)realloc(this->items, this->size * sizeof(int));
-    size_t i = 0;
-    while(s.items[i]){
-        this->items[i] = s.items[i];
-        cout << "Copied item no " << i << " = " << this->items[i] << endl;
-        cout << "This address:" << &this->items[i] << ", Original address:" << &s.items[i] << endl;
-        i++;
-    }
-}
-
-Stack& Stack::operator=(const Stack& s) {
-    if(this != &s)
-    {
-        free(this->items);
-        this->top = s.top;
-        if(this->size < s.size){
-            this->size = s.size;
-        }
-        this->items = (int*)realloc(s.items, s.size * sizeof(int));
-        for (int i = 0; i < this->top; i++) {
-            this->items[i] = s.items[i];
-        }
-    }
-    return *this;
-}
-
-bool Stack::isEmpty()
-{
-    return (this->top == 0);
-}
-
-int Stack::pop()
-{
-    if (this->isEmpty()) {
-        cout << "Stack is empty, cannot pop. Aborting" << endl;
-        abort();
-    }
-    return this->items[--this->top];
-}
-
-void Stack::push(int element)
-{
-    if (this->top >= 0 && (size_t)this->top >= this->size) {
-        size_t newSize = this->resize();
-        int* newItems = (int*)realloc(this->items, newSize * sizeof(int));
-        if (newItems) {
-            this->items = newItems;
-            cout << "Stack size updated " << this->size << " -> " << newSize
-                 << endl;
-            this->size = newSize;
-        }
-        else {
-            cout << "Error reallocating memory. Aborting" << endl;
-            abort();
-        }
-    }
-    this->items[this->top++] = element;
-}
-
-size_t Stack::resize()
-{
-    size_t newSize;
-    if (this->size == 0) {
-        newSize = 2;
-    }
-    else {
-        newSize = this->size * 2;
-    }
-    return newSize;
-}
-
-*/
 #include "Stack.h"
 #include <iostream>
 #include <cstring>
@@ -105,6 +9,7 @@ Stack::Stack()
     this->top = 0;
     this->size = 0;
     this->items = NULL;
+    cout << "\nBasic constructor called" << endl;
 }
 
 Stack::~Stack()
@@ -114,6 +19,7 @@ Stack::~Stack()
 
 Stack::Stack(const Stack& s)
 {
+    cout << "\nCopy constructor called" << endl;
     this->top = s.top;
     this->size = s.size;
     this->items = NULL;
@@ -126,8 +32,10 @@ Stack::Stack(const Stack& s)
 }
 
 Stack& Stack::operator=(const Stack& s) {
+    cout << "\nAssignment operator called" << endl;
     if(this != &s)
     {
+        cout << "Assignment was called between different objects" << endl;
         free(this->items);
         this->top = s.top;
         if(this->size < s.size){
--- Operator Overload Stack/TestStack.cpp
+++ Operator Overload Stack/TestStack.cpp
@@ -3,28 +3,48 @@
 
 using namespace std;
 
+void passToPrintViaValue(Stack s);
+void passToPrintViaReference(Stack& s);
+
 int main(){
     Stack s1;
     s1.push(1);
     s1.push(2);
     s1.push(3);
-    cout << "\n\nStack 1 info:\n----------------------------------------" << endl;
+    cout << "Stack 1 info:\n----------------------------------------" << endl;
+    s1.printInfo();
+    
+    s1 = s1;
+    cout << "Stack 1 info after assigning it to itself:\n----------------------------------------" << endl;
     s1.printInfo();
     
     Stack s2(s1);
-    cout << "\nStack 2 info after copy constructor from Stack 1:\n----------------------------------------" << endl;
+    cout << "Stack 2 info after copy constructor from Stack 1:\n----------------------------------------" << endl;
     s2.printInfo();
     
     Stack s3 = s1;
-    cout << "\nStack 3 info after assignment operator from Stack 1:\n----------------------------------------" << endl;
+    cout << "Stack 3 info after assignment operator from Stack 1:\n----------------------------------------" << endl;
     s3.printInfo();
     
     Stack s4;
     s4.push(10);
-    cout << "\nStack 4 info after usual constructor and 1 push:\n----------------------------------------" << endl;
+    cout << "Stack 4 info after usual constructor and 1 push:\n----------------------------------------" << endl;
     s4.printInfo();
     s4 = s1;
-    cout << "\nStack 4 info after assignment operator from Stack 1:\n----------------------------------------" << endl;
+    cout << "Stack 4 info after assignment operator from Stack 1:\n----------------------------------------" << endl;
     s4.printInfo();
+    
+    passToPrintViaReference(s1);
+    passToPrintViaValue(s1);
     return 0;
+}
+
+void passToPrintViaValue(Stack s){
+    cout << "\n\nPassing stack by value" << endl;
+    s.printInfo();
+}
+
+void passToPrintViaReference(Stack& s){
+    cout << "\n\nPassing stack by reference" << endl;
+    s.printInfo();
 }
