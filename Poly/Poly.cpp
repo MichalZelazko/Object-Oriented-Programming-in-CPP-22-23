38d5575e72200c7b0804f70fdb6bda56c6f57d40
MichalZelazko <242149@edu.p.lodz.pl>
2022-11-21 13:46:47 +0100

    100% professor approved

--- Poly/Poly.cpp
+++ Poly/Poly.cpp
@@ -2,93 +2,105 @@
 
 using namespace std;
 
-void displayMap(map<int, double> elements){
-    for (auto it = elements.begin(); it != elements.end(); it++) {
-        cout << "degree " << it->first << " coefficient: " << it->second << endl;
+void displayMap(map<int, double> elements)
+{
+    for (auto element = elements.begin(); element != elements.end();
+         element++) {
+        cout << "degree " << element->first
+             << " coefficient: " << element->second << endl;
     }
 }
 
-Poly::Poly() {
+Poly::Poly()
+{
     this->elements[0] = 0;
 }
 
-Poly::Poly(double coefficient) {
+Poly::Poly(double coefficient)
+{
     this->elements[0] = coefficient;
 }
 
-Poly::Poly(const Poly& p) {
-    this->elements = p.elements;
-}
-
-Poly::~Poly() {
-    this->elements.clear();
-}
-
-Poly& Poly::operator=(const Poly& p) {
+Poly& Poly::operator=(const Poly& p)
+{
     this->elements = p.elements;
     return *this;
 }
 
-Poly Poly::operator-() const{
+Poly Poly::operator-() const
+{
     Poly temp;
-    for (auto it = this->elements.begin(); it != this->elements.end(); it++) {
-        temp.elements[it->first] = -1 * it->second;
+    for (auto element = this->elements.begin(); element != this->elements.end();
+         element++) {
+        temp.elements[element->first] = -1 * element->second;
     }
     return temp;
 }
 
-double Poly::operator()(double x) const{
+double Poly::operator()(double x) const
+{
     double result = 0;
-    for (auto it = this->elements.begin(); it != this->elements.end(); it++) {
-        result += it->second * pow(x, it->first);
+    for (auto element = this->elements.begin(); element != this->elements.end();
+         element++) {
+        result += element->second * pow(x, element->first);
     }
     return result;
 }
 
-double& Poly::operator[](int index){
+double& Poly::operator[](int index)
+{
     return this->elements[index];
 }
 
-void Poly::removeZeroCoefficient(int index) {
-    if(this->elements[index] == 0) {
+void Poly::removeZeroCoefficient(int index)
+{
+    if (this->elements[index] == 0) {
         this->elements.erase(index);
     }
 }
 
-Poly& Poly::operator+=(const Poly& other){
-    for (auto element = other.elements.begin(); element != other.elements.end(); element++) {
+Poly& Poly::operator+=(const Poly& other)
+{
+    for (auto element = other.elements.begin(); element != other.elements.end();
+         element++) {
         this->elements[element->first] += element->second;
-        if(element->first != 0) {
+        if (element->first != 0) {
             this->removeZeroCoefficient(element->first);
         }
     }
     return *this;
 }
 
-Poly operator+(const Poly& p1, const Poly& p2) {
-    return Poly(p1) += p2;
+Poly operator+(Poly p1, const Poly& p2)
+{
+    return p1 += p2;
 }
 
-Poly& Poly::operator-=(const Poly& other){
-    for (auto element = other.elements.begin(); element != other.elements.end(); element++) {
+Poly& Poly::operator-=(const Poly& other)
+{
+    for (auto element = other.elements.begin(); element != other.elements.end();
+         element++) {
         this->elements[element->first] -= element->second;
-        if(element->first != 0) {
+        if (element->first != 0) {
             this->removeZeroCoefficient(element->first);
         }
     }
     return *this;
 }
 
-Poly operator-(const Poly& p1, const Poly& p2) {
-    return Poly(p1) -= p2;
+Poly operator-(Poly p1, const Poly& p2)
+{
+    return p1 -= p2;
 }
 
-Poly operator*(const Poly& p1, const Poly& p2) {
+Poly operator*(const Poly& p1, const Poly& p2)
+{
     Poly result;
-    for (auto element1: p1.elements) {
-        for (auto element2: p2.elements) {
-            result[element1.first + element2.first] += element1.second * element2.second;
-            if(element1.first + element2.first != 0) {
+    for (auto element1 : p1.elements) {
+        for (auto element2 : p2.elements) {
+            result[element1.first + element2.first] +=
+                element1.second * element2.second;
+            if (element1.first + element2.first != 0) {
                 result.removeZeroCoefficient(element1.first + element2.first);
             }
         }
@@ -96,33 +108,58 @@ Poly operator*(const Poly& p1, const Poly& p2) {
     return result;
 }
 
-ostream& operator<<(ostream& os, const Poly& p) {
+ostream& firstElementSignDisplay(ostream& os,
+                          const map<int, double>::reverse_iterator element)
+{
+    if (element->second < 0) {
+        os << "-";
+    }
+    return os;
+}
+
+ostream& properDegreeDisplay(ostream& os,
+                             const map<int, double>::reverse_iterator element)
+{
+    if (element->first != 0) {
+        os << abs(element->second) << "x^" << element->first << " ";
+    }
+    else {
+        os << abs(element->second) << " ";
+    }
+    return os;
+}
+
+ostream& followingSignDisplay(ostream& os, const map<int, double>& elements,
+                              const map<int, double>::reverse_iterator element)
+{
+    auto auxElement = element;
+    auxElement++;
+    if (auxElement != elements.rend()) {
+        if (element->second > 0) {
+            os << "+ ";
+        }
+        else {
+            os << "- ";
+        }
+    }
+    return os;
+}
+
+ostream& operator<<(ostream& os, const Poly& p)
+{
     map<int, double> elements = p.elements;
     if (elements.size() == 1 && elements[0] == 0) {
         os << "0";
         return os;
     }
-    auto it = elements.rbegin();
-    if (it->second < 0) {
-        os << "-";
-    }
-    for (auto it = elements.rbegin(); it != elements.rend(); it++) {
-        if(it->second != 0) {
-            if(it->first != 0) {
-                os << abs(it->second) << "x^" << it->first << " ";
-            } else {
-                os << abs(it->second) << " ";
-            }
-            auto aux_it = it;
-            aux_it++;
-            if(aux_it != elements.rend()) {
-                if(aux_it->second > 0) {
-                    os << "+ ";
-                } else {
-                    os << "- ";
-                }
-            }
-        }   
+    auto element = elements.rbegin();
+    firstElementSignDisplay(os, element);
+    for (auto element = elements.rbegin(); element != elements.rend();
+         element++) {
+        if (element->second != 0) {
+            properDegreeDisplay(os, element);
+            followingSignDisplay(os, elements, element);
+        }
     }
     return os;
 }
--- Poly/Poly.h
+++ Poly/Poly.h
@@ -1,30 +1,35 @@
 #pragma once
 
+#include <cmath>
 #include <iostream>
 #include <map>
-#include <algorithm>
-#include <cmath>
 
 using namespace std;
 
-class Poly{
-    private:
-        map<int, double> elements;
-        void removeZeroCoefficient(int index);
-    public:
-        Poly();
-        ~Poly();
-        Poly(double coefficient);
-        Poly(const Poly& p);
-        Poly& operator=(const Poly& p);
-        Poly& operator+=(const Poly& p);
-        Poly& operator-=(const Poly& p);
-        Poly operator-() const;
-        friend Poly operator+(const Poly& p1, const Poly& p2);
-        friend Poly operator-(const Poly& p1, const Poly& p2);
-        friend Poly operator*(const Poly& p1, const Poly& p2);
-        double operator()(double x) const;
-        double& operator[](int index);
-        friend ostream& operator<<(ostream& os, const Poly& p);
+class Poly {
+  private:
+    map<int, double> elements;
+    void removeZeroCoefficient(int index);
+    ostream& firstElementSignDisplay(ostream& os,
+                              const map<int, double>::reverse_iterator element);
+    ostream&
+    properDegreeDisplay(ostream& os,
+                        const map<int, double>::reverse_iterator element);
+    ostream&
+    followingSignDisplay(ostream& os, const map<int, double>& elements,
+                         const map<int, double>::reverse_iterator element);
 
+  public:
+    Poly();
+    Poly(double coefficient);
+    Poly& operator=(const Poly& p);
+    Poly& operator+=(const Poly& p);
+    Poly& operator-=(const Poly& p);
+    Poly operator-() const;
+    friend Poly operator+(Poly p1, const Poly& p2);
+    friend Poly operator-(Poly p1, const Poly& p2);
+    friend Poly operator*(const Poly& p1, const Poly& p2);
+    double operator()(double x) const;
+    double& operator[](int index);
+    friend ostream& operator<<(ostream& os, const Poly& p);
 };
